b63dc690ba9962eb387ed607199fe7d6cd1a344e
s18478 <s18478@pjwstk.edu.pl>
2020-06-16 10:31:39 +0200

    Added models

---
+++ Test2/Models/MusicianTrack.cs
@@ -0,0 +1,13 @@
+﻿namespace Test2.Models
+{
+    public class MusicianTrack
+    {
+        public int IdMusicianTrack { get; set; }
+
+        public int IdTrack { get; set; }
+        public Track Track { get; set; }
+
+        public int IdMusician { get; set; }
+        public Musician Musician { get; set; }
+    }
+}
